6d186320b80269382241b784c6204c41cc3f4ec0
rvergis <rvergis@zynga.com>
2014-03-25 20:38:38 -0700

    cleanup UI

--- gen/codepath/apps/gridimagesearch/R.java
+++ gen/codepath/apps/gridimagesearch/R.java
@@ -1002,7 +1002,7 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f060016;
         public static final int action_menu_presenter=0x7f060017;
         public static final int action_mode_close_button=0x7f060024;
-        public static final int action_settings=0x7f060043;
+        public static final int action_settings=0x7f060047;
         public static final int activity_chooser_view_content=0x7f060025;
         public static final int always=0x7f06000b;
         public static final int beginning=0x7f060011;
@@ -1014,7 +1014,7 @@ containing a value of this type.
         public static final int dropdown=0x7f06000f;
         public static final int edit_query=0x7f060030;
         public static final int end=0x7f060013;
-        public static final int etSiteFilter=0x7f060042;
+        public static final int etSiteFilter=0x7f060046;
         public static final int expand_activities_button=0x7f060026;
         public static final int expanded_menu=0x7f06002c;
         public static final int gvResults=0x7f06003d;
@@ -1026,8 +1026,8 @@ containing a value of this type.
         public static final int ivResult=0x7f06003c;
         public static final int listMode=0x7f060001;
         public static final int list_item=0x7f060029;
-        public static final int miSave=0x7f060046;
-        public static final int miSettings=0x7f060045;
+        public static final int miSave=0x7f06004a;
+        public static final int miSettings=0x7f060049;
         public static final int middle=0x7f060012;
         public static final int never=0x7f060009;
         public static final int none=0x7f060010;
@@ -1035,7 +1035,7 @@ containing a value of this type.
         public static final int progress_circular=0x7f060018;
         public static final int progress_horizontal=0x7f060019;
         public static final int radio=0x7f06002f;
-        public static final int search=0x7f060044;
+        public static final int search=0x7f060048;
         public static final int search_badge=0x7f060032;
         public static final int search_bar=0x7f060031;
         public static final int search_button=0x7f060033;
@@ -1050,15 +1050,19 @@ containing a value of this type.
         public static final int showCustom=0x7f060007;
         public static final int showHome=0x7f060004;
         public static final int showTitle=0x7f060006;
-        public static final int spColorFilter=0x7f060041;
-        public static final int spImageSize=0x7f06003f;
-        public static final int spImageType=0x7f060040;
+        public static final int spColorFilter=0x7f060044;
+        public static final int spImageSize=0x7f060040;
+        public static final int spImageType=0x7f060042;
         public static final int split_action_bar=0x7f06001e;
         public static final int submit_area=0x7f060039;
         public static final int tabMode=0x7f060002;
         public static final int title=0x7f06002b;
         public static final int top_action_bar=0x7f060020;
+        public static final int tvColorFilter=0x7f060043;
+        public static final int tvImageSize=0x7f06003f;
+        public static final int tvImageType=0x7f060041;
         public static final int tvNoResults=0x7f06003e;
+        public static final int tvSiteFilter=0x7f060045;
         public static final int up=0x7f060021;
         public static final int useLogo=0x7f060003;
         public static final int withText=0x7f06000c;
@@ -1173,6 +1177,10 @@ containing a value of this type.
         public static final int search_title=0x7f0b0010;
         public static final int title_activity_image_display=0x7f0b0015;
         public static final int title_activity_settings=0x7f0b0013;
+        public static final int tvColorFilter=0x7f0b0018;
+        public static final int tvImageSize=0x7f0b0016;
+        public static final int tvImageType=0x7f0b0017;
+        public static final int tvSiteFilter=0x7f0b0019;
     }
     public static final class style {
         /** 
